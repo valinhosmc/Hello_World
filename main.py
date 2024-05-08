67e3ca5fb70c824859f09e43809468d78cc60593
apidomanutencao <33909646-apidomanutencao@users.noreply.replit.com>
2024-05-08 16:50:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hello World")
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+print("Olá", nome, "e sua idade é", idade)
+print(f"Olá, {nome} sua idade é de {idade}")
